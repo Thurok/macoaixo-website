f78a3e34a8faafe46e1b60941e2c0cc853e34c4e
Pau Guillamon <pauguillamonjaime@gmail.com>
2026-03-29 15:00:08 +0200

    Update site URL and base path in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thurok.github.io',
-  base: '/macoaixo-website',
+  site: 'https://macoaixo.cat',
+  base: '/',
 })
